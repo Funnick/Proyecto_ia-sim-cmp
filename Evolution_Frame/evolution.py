3b21b7d088d66560fbf85b274d977841e6a482ee
Miguel <marhdez1999@gmail.com>
2021-12-06 18:30:58 -0500

    Añade docstring al código existente

--- Evolution_Frame/evolution.py
+++ Evolution_Frame/evolution.py
@@ -4,20 +4,54 @@ from action import DoNothing
 
 
 class Simulator:
+    """
+    Clase principal para representar la simulación. Contiene el mundo y los agentes.
+    """
+
     def __init__(self):
+        """
+        Crea una entidad de simulación sin mundo ni agentes
+        y con 0 días de simulaciones pasadas.
+
+        :rtype: Simulator
+        """
         self.world = None
         self.agents = []
+        self.day = 0
 
     def create_world(self, dimension_x, dimension_y):
+        """
+        Inicializa el mundo de la simulación con tamaño dimension_x * dimension_y.
+
+        :param dimension_x: largo del mundo
+        :type dimension_x: int
+        :param dimension_y: ancho del mundo
+        :type dimension_y: int
+
+        :rtype: None
+        """
         self.world = world.World(dimension_x, dimension_y)
 
-    def create_agent(self, sense, max_energy):
+    def create_agent(self, max_energy, sense):
+        """
+        Añade un nuevo agente a la simulación.
+
+        :param agent: instancia de un agente
+        :type agent: Agent
+
+        :rtype: None
+        """
         r, c = self.world.get_pos_random_edge()
-        ag = agent.Agent(r, c, sense, max_energy)
+        ag = agent.Agent(r, c, max_energy, sense)
         self.agents.append(ag)
         self.world.add_agent(r, c, ag)
 
     def simulate_one_agent_action(self):
+        """
+        Ejecuta una acción por cada agente.
+
+        :rtype: None
+        """
         actions = []
         for ag in self.agents:
             actions.append(ag.play(ag.see(self.world)))
@@ -31,6 +65,11 @@ class Simulator:
         return all_do_nothing_actions
 
     def eliminate_poorly_positioned_hungry_agents(self):
+        """
+        Elimina de la simulación a los agentes que no sobrevivieron al día.
+
+        :rtype: None
+        """
         for ag in self.agents:
             if not (
                 self.world.cell_is_edge(ag.pos_x, ag.pos_y) and ag.food_eat_today >= 1
@@ -39,6 +78,11 @@ class Simulator:
                 self.agents.remove(ag)
 
     def replicate_agents(self):
+        """
+        Reproduce los agentes que cumplieron la condición de reproducción.
+
+        :rtype: None
+        """
         for ag in self.agents:
             if ag.food_eat_today == 2:
                 self.create_agent(3, 10)
@@ -49,8 +93,14 @@ class Simulator:
             ag.current_energy = ag.max_energy
 
     def simulate_one_round(self):
+        """
+        Corre un día completo de la simulación.
+
+        :rtype: None
+        """
+        self.day = self.day + 1
+
         while not self.simulate_one_agent_action():
-            print(self.world)
             pass
 
         self.eliminate_poorly_positioned_hungry_agents()
@@ -58,3 +108,25 @@ class Simulator:
         self.replicate_agents()
 
         self.reset_agents_attributes()
+
+    def get_simulation_day(self):
+        """
+        Devuelve el día actual de la simulación.
+
+        :rtype: int
+        :return: self.day
+        """
+        return self.day
+
+    def get_number_of_agents(self):
+        """
+        Devuelve la cantidad de agentes que hay en la simulación.
+
+        :rtype: int
+        :return: len(self.agents)
+        """
+        return len(self.agents)
+
+    def get_statistics(self):
+        print("Día ->", self.get_simulation_day())
+        print("Número de agentes ->", self.get_number_of_agents())
